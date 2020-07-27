7749c8c0037eb733243c2703678f1c1058bb1ce1
Arcoos2602 <cordonnier.thomas2@gmail.com>
2020-07-27 20:27:59 +0200

    edit en cours

---
+++ lib/gossip.rb
@@ -0,0 +1,33 @@
+require 'csv'
+
+class Gossip
+    attr_accessor :author, :content
+
+    def initialize(author, content)
+      @author = author
+      @content = content
+    end
+
+    def save
+      CSV.open("db/gossip.csv", "a+") do |csv|
+        csv << [@author, @content]
+      end
+    end
+
+    def self.all
+      gossips = []
+      CSV.foreach('./db/gossip.csv') { |row| gossips << Gossip.new(row[0], row[1]) }
+      gossips
+    end
+
+    def self.find(id)
+		  gossips = self.all
+		  gossips[id.to_i]
+    end
+
+    def self.update(id, author, content)
+      gossips = self.all
+      gossips[id.to_i].author = author
+      gossips[id.to_i].content = content
+    end
+end
